69adfd28795109ed14b1447a4f1cdf7ba67e2b15
EmmetZ <Emmet_Z@outlook.com>
2024-04-12 11:25:32 +0800

    Add Summary component

---
+++ src/components/summary.tsx
@@ -0,0 +1,61 @@
+import { Button, GetRef } from 'antd';
+import Paragraph from 'antd/es/typography/Paragraph';
+import { useEffect, useRef, useState } from 'react';
+import { transSummary } from '../services/utils';
+
+interface Props {
+  text: string;
+}
+
+type ParaRef = GetRef<typeof Paragraph>;
+
+const Summary = ({ text }: Props) => {
+  const phrase = transSummary(text);
+  const [collapsed, setCollapse] = useState(false);
+  const [lineCount, setLineCount] = useState(0);
+  const paraRef = useRef<ParaRef>(null);
+
+  useEffect(() => {
+    if (paraRef.current) {
+      const lineHeight = parseFloat(
+        getComputedStyle(paraRef.current).lineHeight
+      );
+      const height = paraRef.current.getBoundingClientRect().height;
+      const lc = Math.floor(height / lineHeight);
+      setLineCount(lc);
+      if (lc > 10) setCollapse(true);
+    }
+  }, [text]);
+  return (
+    <>
+      <Paragraph
+        ref={paraRef}
+        style={{
+          lineHeight: '1.5',
+          maxHeight: collapsed ? '15em' : 'none',
+          overflow: 'hidden',
+          margin: 0,
+        }}
+      >
+        <>
+          {typeof phrase === 'string'
+            ? phrase
+            : phrase.map((p, i) => [p, <br key={i} />])}
+        </>
+      </Paragraph>
+      {lineCount > 10 && (
+        <div style={{ display: 'flex', justifyContent: 'right'}}>
+          <Button
+            type='link'
+            onClick={() => setCollapse(!collapsed)}
+            style={{ padding: 0, margin: '0 20px 0 auto' }}
+          >
+            {collapsed ? 'more...' : 'less⬆'}
+          </Button>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Summary;
